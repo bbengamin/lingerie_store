d361d63b34210715ca4db0756bc603b9f60947b9
Ihor Bohdanov <Ihor_Bohdanov@epam.com>
2016-03-04 21:35:57 +0200

    Install commit

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://test1.ru/admin/');
+define('HTTP_CATALOG', 'http://test1.ru/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://test1.ru/admin/');
+define('HTTPS_CATALOG', 'http://test1.ru/');
+
+// DIR
+define('DIR_APPLICATION', 'S:/home/test1.ru/www/admin/');
+define('DIR_SYSTEM', 'S:/home/test1.ru/www/system/');
+define('DIR_LANGUAGE', 'S:/home/test1.ru/www/admin/language/');
+define('DIR_TEMPLATE', 'S:/home/test1.ru/www/admin/view/template/');
+define('DIR_CONFIG', 'S:/home/test1.ru/www/system/config/');
+define('DIR_IMAGE', 'S:/home/test1.ru/www/image/');
+define('DIR_CACHE', 'S:/home/test1.ru/www/system/storage/cache/');
+define('DIR_DOWNLOAD', 'S:/home/test1.ru/www/system/storage/download/');
+define('DIR_LOGS', 'S:/home/test1.ru/www/system/storage/logs/');
+define('DIR_MODIFICATION', 'S:/home/test1.ru/www/system/storage/modification/');
+define('DIR_UPLOAD', 'S:/home/test1.ru/www/system/storage/upload/');
+define('DIR_CATALOG', 'S:/home/test1.ru/www/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'lingerie_store');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
