fe2fa7c8309bd6698f99d56e44eb09d029413855
Ubuntu <ubuntu@ip-172-31-46-10.us-west-2.compute.internal>
2016-03-29 17:54:24 +0000

    Server inti commit

--- admin/config.php
+++ admin/config.php
@@ -1,31 +1,31 @@
 <?php
 // HTTP
-define('HTTP_SERVER', 'http://test1.ru/admin/');
-define('HTTP_CATALOG', 'http://test1.ru/');
+define('HTTP_SERVER', 'http://52.35.91.62/admin/');
+define('HTTP_CATALOG', 'http://52.35.91.62/');
 
 // HTTPS
-define('HTTPS_SERVER', 'http://test1.ru/admin/');
-define('HTTPS_CATALOG', 'http://test1.ru/');
+define('HTTPS_SERVER', 'http://52.35.91.62/admin/');
+define('HTTPS_CATALOG', 'http://52.35.91.62/');
 
 // DIR
-define('DIR_APPLICATION', 'S:/home/test1.ru/www/admin/');
-define('DIR_SYSTEM', 'S:/home/test1.ru/www/system/');
-define('DIR_LANGUAGE', 'S:/home/test1.ru/www/admin/language/');
-define('DIR_TEMPLATE', 'S:/home/test1.ru/www/admin/view/template/');
-define('DIR_CONFIG', 'S:/home/test1.ru/www/system/config/');
-define('DIR_IMAGE', 'S:/home/test1.ru/www/image/');
-define('DIR_CACHE', 'S:/home/test1.ru/www/system/storage/cache/');
-define('DIR_DOWNLOAD', 'S:/home/test1.ru/www/system/storage/download/');
-define('DIR_LOGS', 'S:/home/test1.ru/www/system/storage/logs/');
-define('DIR_MODIFICATION', 'S:/home/test1.ru/www/system/storage/modification/');
-define('DIR_UPLOAD', 'S:/home/test1.ru/www/system/storage/upload/');
-define('DIR_CATALOG', 'S:/home/test1.ru/www/catalog/');
+define('DIR_APPLICATION', '/var/www/html/admin/');
+define('DIR_SYSTEM', '/var/www/html/system/');
+define('DIR_LANGUAGE', '/var/www/html/admin/language/');
+define('DIR_TEMPLATE', '/var/www/html/admin/view/template/');
+define('DIR_CONFIG', '/var/www/html/system/config/');
+define('DIR_IMAGE', '/var/www/html/image/');
+define('DIR_CACHE', '/var/www/html/system/storage/cache/');
+define('DIR_DOWNLOAD', '/var/www/html/system/storage/download/');
+define('DIR_LOGS', '/var/www/html/system/storage/logs/');
+define('DIR_MODIFICATION', '/var/www/html/system/storage/modification/');
+define('DIR_UPLOAD', '/var/www/html/system/storage/upload/');
+define('DIR_CATALOG', '/var/www/html/catalog/');
 
 // DB
 define('DB_DRIVER', 'mysqli');
 define('DB_HOSTNAME', 'localhost');
 define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', 'root');
 define('DB_DATABASE', 'lingerie_store');
 define('DB_PORT', '3306');
 define('DB_PREFIX', 'oc_');
